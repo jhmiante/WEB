e55d89ea2b1a0a388de4c0cd6197e63b904ddbef
jhmiante <jhmiante@gmail.com>
2020-11-05 21:35:26 -0300

    Js - Condicional IF ... ELSE IF ... ELSE

--- CST - Análise e Desenvolvimento de Sistemas/JsFile.js
+++ CST - Análise e Desenvolvimento de Sistemas/JsFile.js
@@ -34,3 +34,15 @@ if (a == 10)
 	{
 		console.log("Teste IF não aceito, irá ler o bloco Else")	
 	}
+
+
+var data = new Date()
+var hora = data.getHours()
+
+if (hora < 12 ) {
+	console.log("Bom dia!!!")
+}else if (hora < 18){
+	console.log("Boa tarde!!!")
+}else{
+	console.log("Boa Noite!!!")
+}
