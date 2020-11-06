3bcaa7742c79de186090f0a94470d087d8f77d23
jhmiante <jhmiante@gmail.com>
2020-11-05 21:16:01 -0300

    Js - IF

---
+++ CST - Análise e Desenvolvimento de Sistemas/JsFile.js
@@ -0,0 +1,13 @@
+console.log("Condicional IF")
+
+var Num = 5;
+
+if (Num > 4) 
+	{
+		console.log("Teste IF aceito, lendo o Bloco IF")
+	}
+
+if (Num == 10) 
+	{
+		console.log("Teste IF não aceito, Não irá o Bloco IF")
+	}
