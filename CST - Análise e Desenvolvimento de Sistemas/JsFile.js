4637e0550096b998fd3526428b96c868cf17e774
jhmiante <jhmiante@gmail.com>
2020-11-05 21:22:59 -0300

    Js - IF ... ELSE

--- CST - Análise e Desenvolvimento de Sistemas/JsFile.js
+++ CST - Análise e Desenvolvimento de Sistemas/JsFile.js
@@ -11,3 +11,26 @@ if (Num == 10)
 	{
 		console.log("Teste IF não aceito, Não irá o Bloco IF")
 	}
+
+
+console.log("Condicional IF ... ELSE")
+
+var a = 5;
+
+if (a > 4) 
+	{
+		console.log("Teste IF aceito, lendo o Bloco IF")
+	} 
+	else 
+	{
+		console.log("Teste IF aceito, Não irá ler o bloco Else")	
+	}
+
+if (a == 10) 
+	{
+		console.log("Teste IF não aceito, Não irá o Bloco IF")
+	}
+	else 
+	{
+		console.log("Teste IF não aceito, irá ler o bloco Else")	
+	}
