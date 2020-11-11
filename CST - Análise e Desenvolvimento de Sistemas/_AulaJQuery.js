7c3a1dd6be0bb2dafa5dee5990900fa7f12b6220
jhmiante <jhmiante@gmail.com>
2020-11-10 22:26:26 -0300

    jQuery - Eventos

--- CST - Análise e Desenvolvimento de Sistemas/_AulaJQuery.js
+++ CST - Análise e Desenvolvimento de Sistemas/_AulaJQuery.js
@@ -1,1 +1,20 @@
 $("h3.ClassX").text("Combinando Seletores!");
+
+$(document).ready(function(){
+	$("#Elemento").text("Texto Alterado pelo jQuery");
+});
+
+
+var click = 1;
+
+$(document).ready(function(){
+	$("#btnHide").click(function(){
+		if (click == 1) {
+			$("#div1").hide();
+			click = 0;
+		}else{
+			$("#div1").show();
+			click = 1;
+		}
+	});
+});
